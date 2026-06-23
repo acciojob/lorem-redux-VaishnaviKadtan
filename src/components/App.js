a7ee07d350d8e949cd0b15543738da5277e3d94e
VaishnaviKadtan <deepikakadtan15@gmail.com>
2026-06-23 12:24:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,42 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        {
+
+
+          import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchLorem } from "./redux/actions";
+
+export default function App() {
+  const dispatch = useDispatch();
+
+  const { loading, data, error } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(fetchLorem());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h2>Lorem Redux</h2>
+
+      {loading && <p>Loading...</p>}
+
+      {error && <p>{error}</p>}
+
+      {data && (
+        <p>
+          <strong>{data.title}</strong> <br />
+          {data.body}
+        </p>
+      )}
+    </div>
+  );
+}
+
+          
+        }
     </div>
   )
 }
